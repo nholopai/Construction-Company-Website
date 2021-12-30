b28cba2fd96357a9106359ea16dd14cdf9359b78
Nicolas Holopainen <Nic@Nicolass-MacBook-Pro-3.local>
2021-12-29 17:16:28 -0800

    commit

--- script.js
+++ script.js
@@ -3,6 +3,24 @@ const open = document.getElementById("open");
 const nav = document.getElementById("nav");
 const navLinks = document.getElementById("nav-links");
 
-open.addEventListener("click", function() {
+// Click button to toggle down nav
+open.addEventListener("click", () => {
   navLinks.classList.toggle("nav-hidden");
+});
+
+// Hide navigation when scrolling down
+
+window.addEventListener("scroll", () => {
+  if (navLinks.classList.contains("nav-hidden")) {
+    navLinks.classList.remove("nav-hidden");
+  }
+});
+
+// Mobile view back to top button
+
+const upButton = document.getElementById("up");
+
+upButton.addEventListener("click", () => {
+  document.body.scrollTop = 0; // Safari
+  document.documentElement.scrollTop = 0; // Chrome, Firefox etc.
 });
